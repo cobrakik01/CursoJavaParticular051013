10e3bdbffe73b586cf9adfc1370d8076945dae77
Federico Farías Sánchez <cobrakik01@hotmail.com>
2013-10-19 17:37:58 -0500

    Inicio de curso, desde la tersera sesion

--- src/com/cbk/curso/ejercicio03.java
+++ src/com/cbk/curso/ejercicio03.java
@@ -15,5 +15,6 @@ public class ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo!!!");
     }
 }
